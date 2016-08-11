9b1136711c167f339bb9fd802237dad095782367
Junior <Junior@DESKTOP-1JR6GHI>
2016-08-11 14:39:29 -0300

    implementando clienteResouce

--- src/main/java/com/pos/vendaposws/resouces/ClienteResources.java
+++ src/main/java/com/pos/vendaposws/resouces/ClienteResources.java
@@ -16,6 +16,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,6 +26,7 @@ import javax.ws.rs.core.Response;
  * @author Junior
  */
 @Path("cliente")
+@Produces(MediaType.APPLICATION_XML)
 public class ClienteResources {
     
     /**
