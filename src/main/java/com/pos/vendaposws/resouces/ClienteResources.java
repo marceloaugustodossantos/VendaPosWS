aafd0b6a8be317910febeadb2d61d9e16cc9b976
Junior <Junior@DESKTOP-1JR6GHI>
2016-08-11 16:01:49 -0300

    correcoes em clienteresoush

--- src/main/java/com/pos/vendaposws/resouces/ClienteResources.java
+++ src/main/java/com/pos/vendaposws/resouces/ClienteResources.java
@@ -8,9 +8,9 @@ package com.pos.vendaposws.resouces;
 import com.pos.vendapos.valueObjects.Cliente;
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -29,31 +29,45 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_XML)
 public class ClienteResources {
     
-    /**
-     *
-     * @param cliente
-     */
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response addCliente(@FormParam("produto") Cliente cliente){
-        // add cliente no banco
-        return Response.ok(cliente).build();
+    public Response addCliente(@BeanParam Cliente cliente){
+        List<Cliente> clientes = this.getClientes();
+        clientes.add(cliente);
+        GenericEntity<List<Cliente>> gClientes;
+        gClientes = new GenericEntity<List<Cliente>>(clientes){};
+        return Response.ok(gClientes).build();
     }
+    
     @DELETE
     @Path("{id}")
     public Response delCliente(@PathParam("id") long id){
-        Cliente cliente = new Cliente();
-    // recupere o cliente e retorne a interface;    
-    // remover cliente no banco
-        return Response.ok(cliente).build();
+        List<Cliente> clientes = this.getClientes();
+        Cliente cliente = clientes.get((int) (id-1));
+        clientes.remove(cliente);
+        GenericEntity<List<Cliente>> gClientes;
+        gClientes = new GenericEntity<List<Cliente>>(clientes){};
+        return Response.ok(gClientes).build();
     }
+    
     @GET
     public Response listar(){
-        List<Cliente> clientes = new ArrayList<>();
+        List<Cliente> clientes = this.getClientes();
         // recupere a lista de clientes
-        GenericEntity<List<Cliente>> gClientes = new GenericEntity<>(clientes,Cliente.class);
-        return Response.ok( gClientes).build();
+        GenericEntity<List<Cliente>> gClientes;
+        gClientes = new GenericEntity<List<Cliente>>(clientes){};
+        return Response.ok(gClientes).build();
     }
+    
+    private List<Cliente> getClientes(){
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente(1, "Antonio"));
+        clientes.add(new Cliente(2, "Marcelo"));
+        clientes.add(new Cliente(3, "Cleonte"));
+        clientes.add(new Cliente(4, "Ricardo"));
+        return clientes;
+    }
+    
     @PUT
     @Consumes(MediaType.APPLICATION_XML)
     public Response atualizarCliente(Cliente cliente){
